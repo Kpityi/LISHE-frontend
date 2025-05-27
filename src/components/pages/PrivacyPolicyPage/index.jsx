3d5c8ffd01ee8fbec744c6886d1a1eb6f6e0ae2b
Kpityi <kpityi83@gmail.com>
2025-05-27 15:08:21 +0200

    Added Privacy Policy

--- src/components/pages/PrivacyPolicyPage/index.jsx
+++ src/components/pages/PrivacyPolicyPage/index.jsx
@@ -12,8 +12,7 @@ const PrivacyPolicyPage = () => {
       <p className="pp-page__text">
         A László István Honvéd Sporthorgász Egyesület weboldala (www.lishe.hu) nem gyűjt és nem tárol személyes
         adatokat. Az oldal kizárólag információs céllal működik, és nem tartalmaz kapcsolatfelvételi űrlapot vagy
-        felhasználói fiókot. Amennyiben kérdése van, kérjük, lépjen velünk kapcsolatba az info@pelda-horgasz.hu e-mail
-        címen.
+        felhasználói fiókot. Amennyiben kérdése van, kérjük, lépjen velünk kapcsolatba az elnok@lishe.hu e-mail címen.
       </p>
     </div>
   );
